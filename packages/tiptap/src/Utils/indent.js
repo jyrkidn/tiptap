12c27f9d0d5fae83d75fb65dbd34f4861e57dadb
Tymotheos Szulc <tim.e.szulc@gmail.com>
2019-12-12 15:10:13 -0500

    fix: eslint ignore import/prefer-default-export

--- packages/tiptap/src/Utils/indent.js
+++ packages/tiptap/src/Utils/indent.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 export const SIZE_PATTERN = /([\d.]+)(px|pt)/i
 export const PT_TO_PX_RATIO = 1.33
 
@@ -8,3 +9,4 @@ export const MIN_INDENT_LEVEL = 0
 export const MAX_INDENT_LEVEL = 7
 
 export const ATTRIBUTE_INDENT = 'data-indent'
+/* eslint-enable import/prefer-default-export */
